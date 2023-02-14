681ee29a43fadbcbc0bf405d3eb4e24b5e6f7ad9
MartaRondan96 <martarondan96@gmail.com>
2023-02-14 11:56:14 +0100

    metodos basicos controllers

--- src/main/java/com/example/demo/controller/RestCategory.java
+++ src/main/java/com/example/demo/controller/RestCategory.java
@@ -1,5 +1,64 @@
 package com.example.demo.controller;
 
-public class RestCategory {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.CategoryDTO;
+import com.example.demo.service.CategoryService;
+
+@RestController
+@RequestMapping("/api")
+public class RestCategory {
+	
+	@Autowired
+	@Qualifier("categoryService")
+	private CategoryService categoryService;
+	
+	//GET Recupera la categoría correspondiente a ese id
+	@GetMapping("/categories/{categoryId}")
+	public ResponseEntity<?> getCategoryResp(@PathVariable int categoryId)
+	{
+		CategoryDTO category=categoryService.findCategoryByIdModel(categoryId);
+		if(category==null)
+			return ResponseEntity.notFound().build();
+		else
+		return ResponseEntity.ok(category);
+	}
+
+	//POST Crea una nueva categoría
+	@PostMapping("/categories")
+	public ResponseEntity<?> insertCategoryNew (@RequestBody CategoryDTO category)
+	{
+		categoryService.addCategory(category);
+		return ResponseEntity.status(HttpStatus.CREATED).body(category);
+		
+	}
+	
+	//PUT Actualiza una categoría
+	@PutMapping("/categories/{categoryId}")
+	public ResponseEntity<?> updateCategoryNew(@RequestBody CategoryDTO category)
+	{
+		return ResponseEntity.ok(categoryService.addCategory(category));
+	}
+	
+	//DELETE Elimina una categoría y todos sus productos (categoría correspondiente a ese id)
+	@DeleteMapping("/categories/{categoryId}")
+	public ResponseEntity<?> deleteCategoryNew(@PathVariable int categoryId)
+	{
+		boolean exists = categoryService.removeCategory(categoryId);
+		if(exists)
+			return ResponseEntity.ok().build();
+		else
+			return ResponseEntity.notFound().build();
+	}
 }
--- src/main/java/com/example/demo/controller/RestProduct.java
+++ src/main/java/com/example/demo/controller/RestProduct.java
@@ -1,5 +1,53 @@
 package com.example.demo.controller;
 
-public class RestProduct {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.ProductDTO;
+import com.example.demo.service.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class RestProduct {
+	@Autowired
+	@Qualifier("productService")
+	private ProductService productService;
+	
+	
+	//GET Recupera el producto correspondiente a ese id
+	@GetMapping("/products/{productId}")
+	public ResponseEntity<?> getProductResp(@PathVariable int productId)
+	{
+		ProductDTO product=productService.findProductByIdModel(productId);
+		if(product==null)
+			return ResponseEntity.notFound().build();
+		else
+		return ResponseEntity.ok(product);
+	}
+	
+	//PUT Actualiza un producto
+	@PutMapping("/products/{productId}")
+	public ResponseEntity<?> updateProductNew(@RequestBody ProductDTO product)
+	{
+		return ResponseEntity.ok(productService.addProduct(product));
+	}
+	
+	//DELETE Elimina una categoría y todos sus productos (categoría correspondiente a ese id)
+	@DeleteMapping("/products/{productId}")
+	public ResponseEntity<?> deleteProductNew(@PathVariable int productId)
+	{
+		boolean exists = productService.removeProduct(productId);
+		if(exists)
+			return ResponseEntity.ok().build();
+		else
+			return ResponseEntity.notFound().build();
+	}
 }
